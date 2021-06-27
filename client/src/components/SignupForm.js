c908435150b8080b050707b3b57d8a2f0a935371
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-27 09:39:08 -0700

    LOGIN_USER mutation working through form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,7 +3,6 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 
 import { ADD_USER } from '../utils/mutations';
-// import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
@@ -13,7 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser /* { error } */] = useMutation(ADD_USER);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
