8790cb5f567c7b431ad2d32b31441d4c754c56c6
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-29 21:00:42 -0700

    updated server/auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,38 +1,41 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'secret';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.headers.authorization || req.query.token || req.query.token ;
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
 
-    // ["Bearer", "<tokenvalue>"]
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
+    // if no token, return request object as is
     if (!token) {
       return req;
     }
 
-    // verify token and get user data out of it
     try {
+      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
     }
 
+    // return updated request object
     return req;
-  },
-  
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
 };
