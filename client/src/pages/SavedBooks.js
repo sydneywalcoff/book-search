93a69066c70afe89724f9e25f512e3b5ee84956e
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-30 20:07:41 -0700

    removeBook function removes a book, but page doesn't re render

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -4,21 +4,23 @@ import { GET_ME } from '../utils/queries';
 import { useQuery, useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
   const { loading, data: meData } = useQuery(GET_ME);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
   const user = meData?.me;
-  // const userDataLength = Object.keys(userData).length;
+  // const userDataLength = user.savedBooks.length;
   
   useEffect(() => {
     const getUserData = async user => {
+      const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+      if (!token) {
+        return false;
+      }
       try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-  
-        if (!token) {
-          return false;
-        }
         const userData = await user;
         setUserData(userData);
       } catch (e) {
@@ -46,6 +48,17 @@ const SavedBooks = () => {
     if (!token) {
       return false;
     }
+
+    try {
+      const updatedUser = await deleteBook({
+        variables: { bookId: bookId }
+      });
+      setUserData(updatedUser)
+
+    } catch(e) {
+      console.log(e)
+    }
+    console.log(user)
   };
   
 
@@ -95,7 +108,7 @@ const SavedBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => console.log('delete me!')}>
+                  <Button className='btn-block btn-danger' onClick={() => removeBook(book.bookId)}>
                     Delete this Book!
                   </Button>
                 </Card.Body>
